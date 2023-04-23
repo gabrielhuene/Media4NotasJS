6c9bbeca7a41febba3a5c04602370ffafe981c8a
Gabriel Huene <gabriellaurence05@gmail.com>
2023-04-23 16:11:44 -0300

    adiciona a solução do exercício

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Escreva as suas notas de todos os bimestres!")
+
+let b1 = prompt("primeira média")
+let b2 = prompt("segunda média")
+let b3 = prompt("terceira média")
+let b4 = prompt("quarta média")
+
+resultado = (b1 + b2 + b3 + b4) / 4
+
+alert(` esse é o resultado ${resultado}`)
